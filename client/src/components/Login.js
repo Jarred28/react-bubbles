0534700024ad4cf5b7d69403f563d542511634a6
jarred richards <jarred2828@gmail.com>
2019-08-23 14:00:43 -0400

    CreatedLogInPage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,63 @@
-import React from "react";
-
-const Login = () => {
-  // make a post request to retrieve a token from the api
+import React, { useState } from 'react';
+ import axios from 'axios' 
+ // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+
+const Login = (props) => {
+  const [login, setLogin] = useState({username: '', password:''})
+
+  
+
+  const changeHandler = (event) => {
+    event.preventDefault();
+    setLogin({
+        ...login, [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("within handleSubmit", login);
+    
+
+    axios.post('http://localhost:5000/api/login', login)
+      .then(response => {
+          console.log(response);
+          localStorage.setItem('token', response.data.payload)
+          props.history.push('/protected')
+      })
+      .catch(err => console.log("error in handlesSub", err.response))
+
+
+    setLogin({username: "", password: ""})
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+    <div className="loginContainer">
+      <h1>Welcome Back!</h1>
+      <h2>Please Log In.</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+            className="name"
+            placeholder="enter username"
+            type="text"
+            value={login.username}
+            name="username"
+            onChange={changeHandler}
+          />
+        <input
+            className="password"
+            placeholder="enter password"
+            type="password"
+            value={login.password}
+            name="password"
+            onChange={changeHandler}
+          />
+        <button type="submit" className="SubmitButton">Connect!</button>
+      </form>
+    </div>
+  )
+}
 
 export default Login;
